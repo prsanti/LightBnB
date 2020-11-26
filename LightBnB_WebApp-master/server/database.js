09026226ddfb98dfbbf3fbcab87a18d4e08fd4a8
Paulo Santiago <p.santiago796@gmail.com>
2020-11-26 17:47:19 +0000

    add getUserWithEmail and id, and addUser functions to work with database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -19,17 +19,13 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`
+    SELECT * FROM users
+    WHERE email = $1
+    `, [email])
+  .then(res => res.rows[0]);
 }
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -38,7 +34,11 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+    SELECT * FROM users
+    WHERE id = $1
+    `, [id])
+  .then(res => res.rows[0]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -49,10 +49,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *
+  `;
+  const values = [user.name, user.email, user.password];
+  return pool.query(queryString, values)
+    .then(res => res.rows[0]);
 }
 exports.addUser = addUser;
 
@@ -79,9 +83,9 @@ exports.getAllReservations = getAllReservations;
 
 const getAllProperties = function(options, limit = 10) {
   return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+    SELECT * FROM properties
+    LIMIT $1
+    `, [limit])
   .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
